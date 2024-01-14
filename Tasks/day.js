67354ec86029588511ad5f18ebc0e195b77a77c9
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 14:40:33 +0100

    added identifier

--- Tasks/day.js
+++ Tasks/day.js
@@ -2,7 +2,7 @@
 // Get day number
 const DAY = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-parseDays = (s) => {
+const parseDays = (s) => {
   let i;
   for (i = 0; i < DAY.length; i++) {
     if (s.startsWith(DAY[i].toLowerCase())) {
