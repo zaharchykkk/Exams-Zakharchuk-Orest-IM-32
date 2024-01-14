9df457c528744f98d3d251f5e15a05cbe6e50b2d
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 15:44:20 +0100

    const and let

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -1,9 +1,9 @@
 'use strict';
 // Return an array without duplicates
 
-DISTINCT = (data) => {
-  A = new Set();
-  w = 0;
+const DISTINCT = (data) => {
+  const A = new Set();
+  let w = 0;
   data.forEach((a) => {
     if (A.has(a)) {
       delete data[w];
