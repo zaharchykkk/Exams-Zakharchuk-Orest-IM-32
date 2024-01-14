b6d5f43c16ab3d53bcc432ed0b5e92bcaa5fff51
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 15:49:49 +0100

    removed unnecessary code

--- Tasks/distinct.js
+++ Tasks/distinct.js
@@ -3,17 +3,14 @@
 
 const distinct = (arr) => {
   const arrSet = new Set();
-  let counter = 0;
   arr.forEach((item) => {
     if (arrSet.has(item)) {
       delete arr[item];
     } else {
       arrSet.add(item);
     }
-    counter++;
   });
-  return arr.filter
-  (x => typeof x === 'number');
+  return arr;
 };
 
 module.exports = distinct;
