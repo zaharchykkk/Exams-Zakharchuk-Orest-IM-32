12b623897ec60f5f67cb4c0e6d360309cca07229
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 14:41:32 +0100

    let i in for

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,8 +3,7 @@
 const DAY = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 const parseDays = (s) => {
-  let i;
-  for (i = 0; i < DAY.length; i++) {
+  for (let i = 0; i < DAY.length; i++) {
     if (s.startsWith(DAY[i].toLowerCase())) {
       return i + 1;
     }
