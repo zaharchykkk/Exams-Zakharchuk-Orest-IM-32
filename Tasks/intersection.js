e2a06c3f4018b3c73e35926bb06cf4d9f7e62224
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 17:04:41 +0100

    Removed extra block statement

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,22 +3,12 @@
 
 const intersection = (object1, object2) => {
   const object1Keys = Object.keys(object1);
-  {
-  }
   for (const key of object1Keys) {
     if (object2[key] === object1[key]) {
       object2[key] = object1[key];
-      {
-      }
     } else {
       delete object1[key];
-      {
-      }
     }
-    {
-    }
-  }
-  {
   }
   return object1;
 };
