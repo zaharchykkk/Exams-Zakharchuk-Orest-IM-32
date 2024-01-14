789db2308cbcbdce26c70afc65a18c4fae9e4f2e
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 14:49:35 +0100

    removed brackets

--- Tasks/day.js
+++ Tasks/day.js
@@ -3,11 +3,8 @@
 const DAY = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
 
 const parseDays = day => {
-  for (let i = 0; i < DAY.length; i++) {
-    if (day.startsWith(DAY[i])) {
-      return i + 1;
-    }
-  }
+  for (let i = 0; i < DAY.length; i++)
+    if (day.startsWith(DAY[i]))  return i + 1;
   return -1;
 };
 
