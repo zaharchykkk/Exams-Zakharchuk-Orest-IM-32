68687c09d4c2cd0895864558167933c3134bb643
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 16:12:42 +0100

    removed extra elem

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,12 +2,7 @@
 // Generate random integer value in given range
 
 const random = (min, max) => {
-  if (max === undefined) {
-    max = min;
-    return Math.floor(Math.random() * (max + 1));
-  } else {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  }
+  return min + Math.floor(Math.random() * (max - min + 1));
 };
 
 module.exports = random;
