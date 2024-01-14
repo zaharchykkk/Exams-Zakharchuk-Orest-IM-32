e162c135bbcc39d98beaff0985a771152e0a4770
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 16:11:12 +0100

    changed name of func

--- Tasks/random.js
+++ Tasks/random.js
@@ -1,7 +1,7 @@
 'use strict';
 // Generate random integer value in given range
 
-Random = (min, max) => {
+const random = (min, max) => {
   if (max === undefined) {
     max = min;
     return Math.floor(Math.random() * (max + 1));
@@ -10,4 +10,4 @@ Random = (min, max) => {
   }
 };
 
-module.exports = Random;
+module.exports = random;
