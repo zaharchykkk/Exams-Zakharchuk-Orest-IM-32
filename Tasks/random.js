48e76934667f27bcf7bd14eba00b5bff4da47c6e
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 16:13:55 +0100

    small changes in code readability

--- Tasks/random.js
+++ Tasks/random.js
@@ -2,7 +2,7 @@
 // Generate random integer value in given range
 
 const random = (min, max) => {
-  return min + Math.floor(Math.random() * (max - min + 1));
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
 module.exports = random;
