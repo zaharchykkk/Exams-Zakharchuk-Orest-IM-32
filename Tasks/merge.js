164a6a69e7204773f069be62fadf9eb2879f8f2c
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 15:25:33 +0100

    small changes

--- Tasks/merge.js
+++ Tasks/merge.js
@@ -4,10 +4,10 @@
 const mergeTwoObjects = (object1, object2) => {
   const object3 = {};
   const addTo = object => {
-    for (const [key,value] of Object.entries(object))
+    for (const [key, value] of Object.entries(object))
       object3[key] = value;
     return object3;
-  }
+  };
   addTo(object1);
   addTo(object2);
   return object3;
