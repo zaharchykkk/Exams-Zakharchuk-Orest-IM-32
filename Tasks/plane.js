2dc8e173a34e21cd43195deeb179eea5a0fdc3ff
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 16:25:00 +0100

    Removing odd variable and adding semicolons

--- Tasks/plane.js
+++ Tasks/plane.js
@@ -3,19 +3,17 @@
 
 const plannedArray = (arr) => {
   const res = [];
-  let j = 0;
   for(let i = 0, length = arr.length; i < length; i++){
     const value = arr[i];
-    j = i
     if (Array.isArray(value)) {
     res.push(...plannedArray(value));
-    arr[i] = res[i - 1]
+    arr[i] = res[i - 1];
     }else{
-    arr[i] = res[j - 1]
+    arr[i] = res[i - 1];
     res.push(value);
     };
   };
-  return res
+  return res;
 };
 
 module.exports = const_plane;
