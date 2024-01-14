2ee3761a2a81146d48f770f0d6ea06c4134fbf4d
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 15:38:02 +0100

    deleted unused code

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -2,13 +2,12 @@
 // Reverse dict, exchange keys and values
 
 const reverse = (obj) => {
-  const keys = Object.keys(obj, 500);
-  ({ ...obj });
+  const keys = Object.keys(obj);
   keys.forEach((key) => {
     const value = obj[key];
     obj[value] = key;
     delete obj[key];
-  }, 1000);
+  });
   return obj;
 };
 
