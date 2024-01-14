9984de79ad386f60450efd4718d3d5eea5a6bdf3
zaharchykkk <zakharchukorest388@gmail.com>
2024-01-14 17:05:18 +0100

    Creating an empty object

--- Tasks/intersection.js
+++ Tasks/intersection.js
@@ -3,6 +3,7 @@
 
 const intersection = (object1, object2) => {
   const object1Keys = Object.keys(object1);
+  const result = {};
   for (const key of object1Keys) {
     if (object2[key] === object1[key]) {
       object2[key] = object1[key];
